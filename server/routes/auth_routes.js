3fe923055f6d1aa2bb1a1e470213e7b70132a6ce
Dimaaasik <nochevca.dimangio@gmail.com>
2023-05-29 22:53:03 +0300

    Добавил JWT токены и авторизацию

--- server/routes/auth_routes.js
+++ server/routes/auth_routes.js
@@ -1,6 +1,8 @@
 const Router = require("express")
 const User = require("../models/user")
 const bcrypt = require("bcryptjs")
+const config = require("config")
+const jwt = require('jsonwebtoken')
 const {check, validationResult} = require("express-validator")
 const router = new Router
 
@@ -32,8 +34,39 @@ router.post('/registration',
 
     }catch (error){
         console.log(error)
+        res.send({message: "Server error"})
     }
 
 })
+
+
+
+router.post('/login',
+    async (req,res) => {
+        try {
+            const {email, password} = req.body
+            const user = await User.findOne({email})
+            if (!user){
+                return res.status(404).json({message: "User not found"})
+            }
+            const isPassValid = bcrypt.compareSync(password, user.password)
+            if (!isPassValid){
+                return res.status(400).json({message: "Wrong password"})
+            }
+            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+            return res.json({
+                token,
+                user: {
+                    id : user.id,
+                    email: user.email,
+                    avatar: user.avatar
+                }
+            })
+        }catch (error){
+            console.log(error)
+            res.send({message: "Server error"})
+        }
+
+    })
 
 module.exports = router
